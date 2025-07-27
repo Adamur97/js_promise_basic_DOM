7b0e77af57893c7635c0bda36281a5ea73022a25
Adam <adamur.ichigo@onet.pl>
2025-07-27 12:57:33 +0200

    make build

---
+++ index.ba8c63fb.js
@@ -0,0 +1,2 @@
+new Promise(function(n,e){n("Dane załadowane pomyślnie")}).then(function(n){document.body.innerText=n}).catch(function(n){document.body.innerText=n.message});
+//# sourceMappingURL=index.ba8c63fb.js.map
